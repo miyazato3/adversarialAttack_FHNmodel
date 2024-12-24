e1b3f546e3f18e4696b2775af06a7946b174a366
miyazato3 <miyazato.kosuke135@mail.kyutech.jp>
2024-12-24 22:35:19 +0900

    FHN結合振動子のダイナミクスに対する敵対的攻撃のプロトタイプを作成
    FHN結合振動子のダイナミクスに対する敵対的攻撃のプロトタイプを作成

---
+++ advFHNOscillator.py
@@ -0,0 +1,125 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from numba import jit
+import argparse
+import os
+
+#### Parameters #############
+parser = argparse.ArgumentParser()
+parser.add_argument('--network', type=str, default="ws_p=0.txt", help='file name of network')
+parser.add_argument('--N', type=int, default=90, help='num of FHN oscillator')
+parser.add_argument('--sigma', type=float, default=0.0506, help='FHN model parameter')
+parser.add_argument('--epsilon', type=float, default=0.05, help='FHN model parameter')
+parser.add_argument('--a', type=float, default=0.5, help='FHN model parameter')
+parser.add_argument('--tmax', type=float, default=10000, help='maximum simulation time')
+parser.add_argument('--dt', type=float, default=0.5, help='size of dt')
+parser.add_argument('--t_interval', type=float, default=1.0, help='perturbation interval')
+parser.add_argument('--attack_eps', type=float, default=0.05, help='strength of perturbation')
+parser.add_argument('--seed', type=int, default=128, help='random seed for reproducibility')
+args = parser.parse_args()
+
+# Kuramoto model
+@jit(nopython=True)
+def fhn_ode(X, N, epsilon, sigma, a, A, B):
+    u = X[:N]
+    v = X[N:]
+    du = np.zeros(N)
+    dv = np.zeros(N)
+    for k in range(N):
+        sum_u = np.sum(A[k, :] * (B[0, 0] * (u - u[k]) + B[0, 1] * (v - v[k])))
+        sum_v = np.sum(A[k, :] * (B[1, 0] * (u - u[k]) + B[1, 1] * (v - v[k])))
+        du[k] = (u[k] - u[k]**3 / 3 - v[k] + sigma * sum_u) / epsilon
+        dv[k] = u[k] + a + sigma * sum_v
+    
+    result = np.zeros(2 * N)
+    result[:N] = du
+    result[N:] = dv
+
+    return result
+
+# wrapper for solve_ivp
+def fhn_wrapper(t, X, N, epsilon, sigma, a, A, B):
+    return fhn_ode(X, N, epsilon, sigma, a, A, B)
+
+def run_fhn_simulation(X0, A, B, attack_eps, dt=0.05):
+    r_values = []
+    t_values = []
+    
+    # pre-running for JIT
+    _ = fhn_ode(X0, args.N, args.epsilon, args.sigma, args.a, A, B)
+    
+    t_current = 0
+    uv_current = X0.copy()
+    
+    while t_current < args.tmax:
+        t_end = round(min(t_current + args.t_interval, args.tmax), 6)
+        t_eval = np.round(np.arange(t_current, t_end, dt), 6)
+
+        sol = solve_ivp(
+            fhn_wrapper,
+            [t_current, t_end],
+            uv_current,
+            t_eval=t_eval,
+            args=(args.N, args.epsilon, args.sigma, args.a, A, B)
+        )
+        
+        uv_current = sol.y[:, -1]
+        u = uv_current[:args.N]
+        v = uv_current[args.N:]
+
+        # add perturbation
+        if t_end < args.tmax:
+            # dr/dgeophi
+            geo_phi = np.arctan2(v, u)
+            psi = np.angle(np.mean(np.exp(1j * geo_phi)))
+            dr_dgeophi = np.sin(psi - geo_phi)
+
+            # dr/du
+            dgeophi_du = -v / (u**2 + v**2)
+            dr_du = dr_dgeophi * dgeophi_du
+            
+            # dr/dv
+            dgeophi_dv = u / (u**2 + v**2)
+            dr_dv = dr_dgeophi * dgeophi_dv
+
+            # add perturbation
+            u = u + attack_eps * np.sign(dr_du)
+            v = v + attack_eps * np.sign(dr_dv)
+            uv_current[:args.N] = u
+            uv_current[args.N:] = v
+            #theta_current = theta_current + attack_eps * np.sign(np.sin(psi - theta_current))
+
+        t_current = t_end
+        t_values.append(t_current)
+    
+        # compute order parameter
+        geo_phase = np.arctan2(v, u)
+        r = np.abs(np.mean(np.exp(1j * geo_phase)))
+        r_values.append(r)
+    
+    return t_values, r_values
+
+if __name__ == "__main__":
+    # set random seed
+    np.random.seed(args.seed)
+    
+    # set A, B
+    A = np.loadtxt(args.network)
+    phi = np.pi/2 - 0.1
+    B = np.array([[np.cos(phi), np.sin(phi)], [-np.sin(phi), np.cos(phi)]])
+
+    # initial u, v
+    u0 = np.random.uniform(-1, 1, size=args.N)
+    v0 = np.random.uniform(-1, 1, size=args.N)
+    X0 = np.concatenate([u0, v0])
+
+    # simulation on FHN Oscillator Model
+    t_values, r_values = run_fhn_simulation(X0, A, B, attack_eps=args.attack_eps, dt=args.dt)
+
+    # save result
+    os.makedirs("results", exist_ok=True)
+    np.savetxt(f"results/t_values_{args.network[:-4]}_eps={args.attack_eps}_seed={args.seed}.txt", t_values)
+    np.savetxt(f"results/r_values_{args.network[:-4]}_eps={args.attack_eps}_seed={args.seed}.txt", r_values)
+    
---
+++ experiment.py
@@ -0,0 +1,60 @@
+import numpy as np
+import subprocess
+import matplotlib.pyplot as plt
+import os
+
+# パラメータ設定
+network_list = ["WSp=0.txt", "WSp=0.006.txt", "WSp=0.232.txt", "WSp=1.txt", "FRACTAL.txt"]
+attack_eps = [0.0, 0.05, -0.05]
+tmax = 10000
+dt = 0.5
+t_interval = 1.0
+seed = 128
+
+os.makedirs("results", exist_ok=True)
+
+# シミュレーション実行
+for network in network_list:
+    for e in attack_eps:
+        out = subprocess.run(
+            [
+                "python", "advFHNOscillator.py",
+                *[
+                    "--network", f"{network}",
+                    "--N", f"{90 if network != 'FRACTAL.txt' else 82}",
+                    "--sigma", f"{0.0506 if network != 'FRACTAL.txt' else 0.01}",
+                    "--epsilon", "0.05",
+                    "--a", "0.5",
+                    "--tmax", f"{tmax}",
+                    "--dt", f"{dt}",
+                    "--t_interval", f"{t_interval}",
+                    "--attack_eps", f"{e}",
+                    "--seed", f"{seed}"
+                ]
+            ],
+            text=True, capture_output=True, check=True
+        )
+        print(out)
+
+    # 結果を読み込む
+    r_values_base = np.loadtxt(f"r_values_{network[:-4]}_eps={attack_eps[0]}_seed={seed}.txt")
+    t_values_base = np.loadtxt(f"t_values_{network[:-4]}_eps={attack_eps[0]}_seed={seed}.txt")
+    r_values_pos = np.loadtxt(f"r_values_{network[:-4]}_eps={attack_eps[1]}_seed={seed}.txt")
+    t_values_pos = np.loadtxt(f"t_values_{network[:-4]}_eps={attack_eps[1]}_seed={seed}.txt")
+    r_values_neg = np.loadtxt(f"r_values_{network[:-4]}_eps={attack_eps[2]}_seed={seed}.txt")
+    t_values_neg = np.loadtxt(f"t_values_{network[:-4]}_eps={attack_eps[2]}_seed={seed}.txt")
+    
+    # 結果をプロット
+    plt.figure(figsize=(8, 4))
+    plt.plot(t_values_base, r_values_base, alpha=0.6, linewidth=0.1, label="$\epsilon=0$")
+    plt.plot(t_values_pos, r_values_pos, alpha=0.6, linewidth=0.1, label="$\epsilon=0.05$")
+    plt.plot(t_values_neg, r_values_neg, alpha=0.6, linewidth=0.1, label="$\epsilon=-0.05$")
+    plt.xlabel("Time $t$")
+    plt.ylabel("Order Parameter $r$")
+    plt.ylim(0,1)
+    legend = plt.legend(loc="lower left", bbox_to_anchor=(1.05, 0), borderaxespad=0.)
+    for legend_line in legend.get_lines():
+        legend_line.set_linewidth(2.0)
+        legend_line.set_alpha(1.0)
+    plt.tight_layout(rect=[0, 0, 0.85, 1])
+    plt.savefig(f"results/{network[:-4]}_tmax{tmax}_dt{dt}_tinterval{t_interval}_seed{seed}.png", dpi=500)
